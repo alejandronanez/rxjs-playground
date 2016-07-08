fdffd2d538ec23e458406e07014660f229b95470
Alejandro Nanez <alejonanez@gmail.com>
2016-07-07 22:39:06 -0500

    Observables 101

--- app/index.js
+++ app/index.js
@@ -1,1 +1,24 @@
-document.body.innerHTML = 'Hello world using Js';
+/* eslint-disable no-console */
+import Rx from 'rxjs';
+
+const simple$ = new Rx.Observable(observer => {
+	console.log('Generating Observable');
+
+	setTimeout(() => {
+		observer.next('An Item!');
+
+		setTimeout(() => {
+			observer.next('Another Item');
+			observer.complete();
+		}, 1000);
+
+	}, 1000);
+});
+
+simple$.subscribe(
+	item => console.log(`On subscribe: one.next ${item}`),
+	error => console.log(`On subscribe: one.error ${error}`),
+	() => console.log('On subscribe: one.complete')
+);
+
+/* eslint-enable no-console */
