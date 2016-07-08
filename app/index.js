740c4577c88dc9f97322310ed2417a667a0bb484
Alejandro Nanez <alejonanez@gmail.com>
2016-07-08 06:35:16 -0500

    Stop interval with unsubscribe

--- app/index.js
+++ app/index.js
@@ -6,11 +6,25 @@ import { createSubscriber } from 'helpers';
 function createInterval(time) {
 	return new Rx.Observable(observer => {
 		let index = 0;
-		setInterval(() => {
+		const interval = setInterval(() => {
+			console.log(`Generating ${index}`);
 			observer.next(index++);
 		}, time);
+
+		return () => {
+			// This will get called when .unsubscribe()
+			cleaInterval(interval);
+		};
 	});
 }
 
 const everySecond$ = createInterval(1000);
-everySecond$.subscribe(createSubscriber('everySecond$'));
+// everySecond$.subscribe(createSubscriber('everySecond$')); -> Print every second in the console
+
+// Print to the console.
+const subscription = everySecond$.subscribe(createSubscriber('everySecond$'));
+
+// Stop printing to the console after 3500ms
+setTimeout(() => {
+	subscription.unsubscribe();
+}, 3500);
