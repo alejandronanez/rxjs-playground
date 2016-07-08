5c6d852ba34d59123c9722e4ef7f7d8c2b4da6d4
Alejandro Nanez <alejonanez@gmail.com>
2016-07-07 23:19:27 -0500

    Created interval

--- app/index.js
+++ app/index.js
@@ -6,9 +6,11 @@ import { createSubscriber } from 'helpers';
 function createInterval(time) {
 	return new Rx.Observable(observer => {
 		let index = 0;
-
 		setInterval(() => {
 			observer.next(index++);
 		}, time);
 	});
 }
+
+const everySecond$ = createInterval(1000);
+everySecond$.subscribe(createSubscriber('everySecond$'));
