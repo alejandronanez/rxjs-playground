48d5aae0a47ad315072ff6c033c63224b971fee8
Alejandro Nanez <alejonanez@gmail.com>
2016-07-07 22:51:00 -0500

    Subscribe to error

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-document.body.innerHTML = 'Hello world using Js';
+
+import Rx from 'rxjs';
+
+const error$ = new Rx.Observable(observer => {
+	observer.error(new Error('My Error Message'));
+});
+
+error$.subscribe(
+	item => console.log(item),
+	error => console.log(`Error: ${error.stack}`)
+);
