da63bbe0dbfa2437f6e0e61d43a7f864237500f6
Alejandro Nanez <alejonanez@gmail.com>
2016-07-08 07:02:34 -0500

    Rough take operator

--- app/index.js
+++ app/index.js
@@ -1,1 +1,42 @@
-document.body.innerHTML = 'Hello world using Js';
+
+import Rx from 'rxjs';
+
+import { createSubscriber } from 'helpers';
+
+function createInterval$(time) {
+	return new Rx.Observable(observer => {
+		let index = 0;
+		const interval = setInterval(() => {
+			console.log(`Generating ${index}`);
+			observer.next(index++);
+		}, time);
+
+		return () => {
+			// This will get called when .unsubscribe()
+			clearInterval(interval);
+		};
+	});
+}
+
+function take$(sourceObservable$, amount) {
+	return new Rx.Observable(observer => {
+		let count = 0;
+
+		const subscription = sourceObservable$.subscribe({
+			next(item) {
+				observer.next(item);
+				if (++count >= amount) {
+					observer.complete();
+				}
+			},
+			error(error) { observer.error(error); },
+			complete() { observer.complete(); }
+		});
+
+		return () => subscription.unsubscribe();
+	});
+}
+
+const everySecond$ = createInterval$(1000);
+const firstFiveSeconds$ = take$(everySecond$, 5);
+const subscription = firstFiveSeconds$.subscribe(createSubscriber('firstFiveSeconds$'));
